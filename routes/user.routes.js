b5bb9c50b11fe67b5461cd298a8050a04a4682ea
BHARAT Chaudhary <bhanuchaudhary709919@gmail.com>
2026-05-05 15:35:03 +0530

    add ecommerce

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,4 +1,9 @@
 import express from "express";
-import { login,register,updateProfile } from "../controller/user.controller.js";
-import isAuthenticated from "../middlewares/isAuthenticated.js";
+import { login,register,updateProfile } from "../controllers/user.controller.js";
+import isAuthenticated from "../middleware/authmiddleware.js";
 const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.put("/update-profile", isAuthenticated, updateProfile);
+export default router;
